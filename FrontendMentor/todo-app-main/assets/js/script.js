b892d3099d1f9f964f17e57f3cb1c807821b7ca2
izabelydev <izabelylrnc@gmail.com>
2024-08-02 18:58:07 -0300

    Ajustes na transição dos temas light e dark; Adicionar e excluir tarefas

--- FrontendMentor/todo-app-main/assets/js/script.js
+++ FrontendMentor/todo-app-main/assets/js/script.js
@@ -1,12 +1,13 @@
+// Início animação mudar tema
 const btnTheme = document.querySelector('.change-theme');
 
 btnTheme.addEventListener('click', () => {
     const imgTheme = document.querySelector('.theme-icon');
 
-    if(imgTheme.classList.contains('moon')) {
+    if (imgTheme.classList.contains('moon')) {
         changeTheme('sun', 'icon-sun.svg', 'Sun');
         darkTheme();
-    } else if(imgTheme.classList.contains('sun')) {
+    } else if (imgTheme.classList.contains('sun')) {
         changeTheme('moon', 'icon-moon.svg', 'Moon');
         lightTheme();
     }
@@ -14,13 +15,13 @@ btnTheme.addEventListener('click', () => {
     function changeTheme(newClass, newImg, newAlt) {
         btnTheme.classList.add('off');
         btnTheme.classList.remove('on');
-    
-        
-        setTimeout(() => {        
+
+
+        setTimeout(() => {
             imgTheme.setAttribute('class', `theme-icon ${newClass}`);
             imgTheme.setAttribute('src', `./assets/images/${newImg}`);
             imgTheme.setAttribute('alt', `${newAlt}`);
-            
+
             btnTheme.classList.remove('off');
             btnTheme.classList.add('on');
         }, 500);
@@ -28,16 +29,18 @@ btnTheme.addEventListener('click', () => {
 });
 
 const secondBg = document.querySelector('.second-bg');
-const allTasksBg = document.querySelector('.tasks');
+const allTasks = document.querySelector('.tasks');
 const filtersBg = document.querySelector('.btn-filters');
 const taskBg = document.querySelectorAll('.task');
+const inputTxt = document.querySelector('.task-txt');
 const btnFilter = document.querySelectorAll('.btn');
 const isMobile = window.innerWidth < 575;
 
 function darkTheme() {
     secondBg.classList.add('dark');
-    allTasksBg.classList.add('dark');
+    allTasks.classList.add('dark');
     filtersBg.classList.add('dark');
+    inputTxt.classList.add('dark');
 
     for (let i = 0; i < taskBg.length; i++) {
         taskBg[i].classList.add('dark');
@@ -50,8 +53,9 @@ function darkTheme() {
 
 function lightTheme() {
     secondBg.classList.remove('dark');
-    allTasksBg.classList.remove('dark');
+    allTasks.classList.remove('dark');
     filtersBg.classList.remove('dark');
+    inputTxt.classList.remove('dark');
 
     for (let i = 0; i < taskBg.length; i++) {
         taskBg[i].classList.remove('dark');
@@ -61,3 +65,60 @@ function lightTheme() {
         btnFilter[i].classList.remove('dark');
     }
 }
+
+// Fim animação mudar tema
+
+// Animação botão para checar tarefa
+
+// Adicionar tarefas
+const inputTask = document.querySelector('#task-input');
+const btnTask = document.querySelector('.add-task');
+// allTasks
+
+function clearInput() {
+    inputTask.value = '';
+    inputTask.focus();
+}
+
+function createTask(txtInput) {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'task off');
+    li.innerHTML = `
+        <button type="button" class="btn-check">
+            <img src="./assets/images/check.svg" alt="Check icon">
+        </button>
+        <p class="task-txt">${txtInput}</p>
+        <img src="./assets/images/close.svg" alt="Remove task"  class="remove-task">`;
+
+    allTasks.appendChild(li);
+    clearInput();
+
+    setTimeout(() => {
+        li.classList.remove('off');
+        li.classList.add('on');
+    }, 100);
+}
+
+btnTask.addEventListener('click', () => {
+    if (!inputTask.value) return;
+    createTask(inputTask.value);
+});
+
+inputTask.addEventListener('keypress', e => {
+    if(e.keyCode === 13) {
+        if (!inputTask.value) return;
+        createTask(inputTask.value);
+    }
+});
+
+document.addEventListener('click', e => {
+    const el = e.target;
+
+    if(el.classList.contains('remove-task')) {
+        el.parentElement.classList.remove('on');
+        el.parentElement.classList.add('off');
+        setTimeout(() => {
+            el.parentElement.remove();
+        }, 200);
+    }
+});
